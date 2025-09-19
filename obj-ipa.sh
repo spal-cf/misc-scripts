210ae755f8494732d558eed6d05f8cb811275565
spal-cf <96542761+spal-cf@users.noreply.github.com>
2025-09-19 19:50:32 -0400

    Update obj-ipa.sh

--- obj-ipa.sh
+++ obj-ipa.sh
@@ -5,17 +5,21 @@ set -v
 app_binary=<binary name> 
 
 objection -g $app_binary run env
+sleep 1
 objection -g $app_binary run ios info binary
+objection -g $app_binary run "ios bundles list_bundles --full-path"
 objection -g $app_binary run ios bundles list_frameworks
 objection -g $app_binary run ios nsuserdefaults get
 objection -g $app_binary run ios nsurlcredentialstorage dump
 objection -g $app_binary run ios plist cat Info.plist
 
-objection -g $app_binary run ios keychain dump
+objection -g $app_binary run "ios keychain dump --json keychain.json"
 objection -g $app_binary run ios cookies get
 
-objection -g $app_binary run  memory list modules
+objection -g $app_binary run  memory list exports
 
+objection -g $app_binary run  "memory list modules --json modules.json"
+sleep 1
 objection -g $app_binary run ios hooking search classes cloudbi
 
 
